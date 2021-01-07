843c45fdb05219c7146a8f7ee24606f067c3c7ff
JGMEYER <meyer.justian@gmail.com>
2021-01-06 20:46:07 -0500

    Move vehicles smoothly between nodes

--- src/road/Vehicle.js
+++ src/road/Vehicle.js
@@ -11,6 +11,7 @@ export class Vehicle {
         this.color = this.randomColor();
         this.path = [];
         this.prevTargetId = startNodeId;
+        this.speed = 5;
     }
 
     randomColor() {
@@ -28,23 +29,58 @@ export class Vehicle {
         this.path = path;
     }
 
+    setRandomPath(travelGraph) {
+        const randPath = this._getRandomPath(travelGraph);
+        this.setPath(randPath);
+    }
+
     _getRandomPath(travelGraph) {
         const randTarget = travelGraph.getRandomNode();
         return travelGraph.getShortestPath(this.prevTargetId, randTarget.id);
     }
 
+    _moveTowardsTarget(remainingSpeed, targetNode) {
+        const x1 = this.x;
+        const y1 = this.y;
+        const x2 = targetNode.x;
+        const y2 = targetNode.y;
+        const dx = x2 - x1;
+        const dy = y2 - y1;;
+
+        const distToNode = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+
+        const speed = Math.min(this.speed, distToNode);
+        const angle = Math.atan2(dy, dx);
+        const xVelocity = speed * Math.cos(angle);
+        const yVelocity = speed * Math.sin(angle);
+
+        this.x += Math.floor(xVelocity);
+        this.y += Math.floor(yVelocity);
+
+        return remainingSpeed - speed;
+    }
+
     step(travelGraph) {
+        // No where else to go
         if (this.path.length === 0) {
-            const randPath = this._getRandomPath(travelGraph);
-            this.setPath(randPath);
+            this.setRandomPath(travelGraph);
         }
 
-        const targetId = this.path.shift();
-        const targetNode = travelGraph.getNode(targetId);
+        let targetNode = travelGraph.getNode(this.path[0])
+        let remainingSpeed = this.speed;
 
-        this.prevTargetId = parseInt(targetId);
-        this.x = targetNode.x;
-        this.y = targetNode.y;
+        while (remainingSpeed > 0) {
+            remainingSpeed = this._moveTowardsTarget(remainingSpeed, targetNode);
+
+            if (this.x === targetNode.x && this.y === targetNode.y) {
+                this.prevTargetId = parseInt(this.path.shift());
+                // Repopulate path if exhausted
+                if (this.path.length === 0) {
+                    this.setRandomPath(travelGraph);
+                }
+                targetNode = travelGraph.getNode(this.path[0]);
+            }
+        }
     }
 }
 
